171737d7d73aa0dba5a132c0bda4dd94fb5e8e9e
kionpark <kionpark@gmail.com>
2024-10-30 17:55:59 +0900

    sz

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
 const express = require('express')
 const app = express();
+const { MongoClient } = require('mongodb')
+
+let db
+const url = 'mongodb사이트에 있던 님들의 DB 접속 URL'
+new MongoClient(url).connect().then((client)=>{
+  console.log('DB연결성공')
+  db = client.db('forum')
+}).catch((err)=>{
+  console.log(err)
+})
+
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (요청, 응답) => {
