42d13c842be7d31bf9bf8b20652992a7e5c8cb6b
kionpark <kionpark@gmail.com>
2024-10-31 17:49:01 +0900

    시발 왜 안되지

--- server.js
+++ server.js
@@ -2,16 +2,28 @@ const express = require('express')
 const app = express();
 const { MongoClient } = require('mongodb')
 
-let db
-const url = 'mongodb사이트에 있던 님들의 DB 접속 URL'
-new MongoClient(url).connect().then((client)=>{
-  console.log('DB연결성공')
-  db = client.db('forum')
-}).catch((err)=>{
-  console.log(err)
+
+
+
+let db;
+const id = encodeURIComponent("mincj93");
+const pw = encodeURIComponent("AlsCkd!@34");
+
+const url = `mongodb+srv://${id}:${pw}@cluster0.xuzu2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+new MongoClient(url).connect().then((client) => {
+    console.log('DB연결성공')
+    db = client.db('forum')
+}).catch((err) => {
+    console.log(err)
 })
 
+
+// Express 설정
 app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'ejs')
+
+
+
 
 app.get('/', (요청, 응답) => {
     응답.send('반갑다')
@@ -26,6 +38,9 @@ app.get('/pageOne', (요청, 응답) => {
 })
 app.get('/pageTwo', (요청, 응답) => {
     응답.sendFile(__dirname + "/pages/pageTwo.html")
+})
+app.get('/news', () => {
+    db.collection('post').insertOne({ title: 'testtest' })
 })
 
 app.get('/test', (요청, 응답) => {
